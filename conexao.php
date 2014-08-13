0f903656edf19dae8c6af4a823a1664cc1b07ce3
guilhermemaster <guilhermebali@gmail.com>
2014-08-13 19:53:02 -0300

    Revert "Revert "php""
    This reverts commit d820730bc9aeb0317c2965892fea5e4f0c3a3b75.

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+$conexao = mysql_connect("mysql4.000webhost.com", "a7458076_admin", "caridade100")	 or die ("Erro na conexão com o bd");
+					$db = mysql_select_db("a7458076_prime") or die ("Erro na seleção da base de dados"); 
+
+?>
