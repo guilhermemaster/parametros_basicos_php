4e486939c2d3a4a5d907f906d9591c567da599bf
guilherme barbosa <guilhermebali@gmail.com>
2014-11-10 19:21:38 -0200

    ajuste básico

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
 $conexao = mysql_connect("localhost", "root", "leticia")	 or die ("Erro na conexão com o bd");
-					$db = mysql_select_db("Centroespi") or die ("Erro na seleção da base de dados"); 
+					$db = mysql_select_db("Centroespi") or die ("Erro na seleção da base de dados!"); 
 
 ?>
